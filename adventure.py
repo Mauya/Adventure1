c151c514def63207a35876d301e509f64bd2ae1d
Mauya <smkadungure@live.co.uk>
2017-01-28 19:26:14 +0000

    fixes #1 check if user has entered a valid direction

--- adventure.py
+++ adventure.py
@@ -13,7 +13,7 @@ while True:
     location = locations[position]
     print 'you are at the %s' % location
 
-    valid_directions = {}
+    valid_directions = {-1, 0}
     for k, v in directions.iteritems():
         possible_position = (position[0] + v[0], position[1] + v[1])
         possible_location = locations.get(possible_position)
@@ -22,4 +22,9 @@ while True:
             valid_directions[k] = possible_position
 
     direction = raw_input('which direction do you want to go?\n')
-    position = valid_directions[direction]
+
+        new_position = valid_directions.get(direction)
+        if new_position:
+            position = new_position
+        else:
+            print "sorry, that isn't a valid direction"
